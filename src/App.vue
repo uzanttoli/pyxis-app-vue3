fbcd9315f38510b4efeb859306a8b82927812bfa
uzanttoli <uandersonphelps70@gmail.com>
2025-11-18 23:54:12 -0300

    criado menu

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import DefaultLayout from './layouts/DefaultLayout.vue'
 // import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-
-  <RouterView />
+  <DefaultLayout>
+    <RouterView />
+  </DefaultLayout>
 </template>
 
 <style scoped>
--- src/layouts/components/AppSidebar.vue
+++ src/layouts/components/AppSidebar.vue
@@ -8,19 +8,45 @@ defineEmits(['close'])
 <template>
   <div
     v-if="open"
-    class="fixed inset-0 bg-black bg-opacity-40 md:hidden"
+    class="fixed inset-0 bg-black/40 md:hidden"
     @click="$emit('close')"
   ></div>
 
   <aside
-    class="fixed md:static top-0 left-0 h-full w-64 bg-white border-r shadow-sm transform transition-transform duration-200 md:translate-x-0 flex flex-col"
+    class="fixed md:static top-0 left-0 h-full w-72 bg-white/10 backdrop-blur-xl border border-white/20 shadow-xl transform transition-transform duration-200 md:translate-x-0 flex flex-col md:rounded-2xl md:m-4 relative"
     :class="open ? 'translate-x-0' : '-translate-x-full'"
   >
-    <div class="h-16 flex items-center px-4 border-b font-semibold">Menu</div>
+    <div class="relative h-16 flex items-center px-4 border-b border-white/20">
+      <img src="@/assets/logo.svg" alt="Logo Pyxis" class="w-8 h-8 mr-2" />
+      <span class="font-semibold">Pyxis App</span>
+      <div class="pointer-events-none absolute inset-0 bg-gradient-to-br from-white/15 via-transparent to-white/5"></div>
+    </div>
 
     <nav class="flex-1 p-4 space-y-2">
-      <a href="#" class="block px-3 py-2 rounded bg-gray-100">teste 1</a>
-      <a href="#" class="block px-3 py-2 rounded bg-gray-100">teste 2</a>
+      <RouterLink
+        to="/ocupacao"
+        class="group relative block px-3 py-2 rounded-xl bg-white/5 hover:bg-white/10 border border-white/20 shadow-sm transition"
+      >
+        Ocupação
+        <span
+          class="absolute left-full top-1/2 -translate-y-1/2 ml-2 hidden group-hover:block px-2 py-1 text-xs rounded-lg bg-black/60 text-white backdrop-blur-md border border-white/20 shadow"
+        >
+          Página inicial
+        </span>
+      </RouterLink>
+
+      <RouterLink
+        to="/about"
+        class="group relative block px-3 py-2 rounded-xl bg-white/5 hover:bg-white/10 border border-white/20 shadow-sm transition"
+      >
+        Sobre
+        <span
+          class="absolute left-full top-1/2 -translate-y-1/2 ml-2 hidden group-hover:block px-2 py-1 text-xs rounded-lg bg-black/60 text-white backdrop-blur-md border border-white/20 shadow"
+        >
+          Sobre o app
+        </span>
+      </RouterLink>
+
     </nav>
   </aside>
 </template>
