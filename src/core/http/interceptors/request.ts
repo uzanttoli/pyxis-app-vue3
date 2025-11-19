6fa6fc8f03c6e6ec579bba48cc9aca6ea599b759
Uanderson de Oliveira dos Santos <93135373+uzanttoli@users.noreply.github.com>
2025-11-19 16:43:15 -0300

    estruturando appsidebar

--- src/core/http/interceptors/request.ts
+++ src/core/http/interceptors/request.ts
@@ -1,7 +1,9 @@
 export default function setupRequestInterceptors(http: any) {
   http.interceptors.request.use(
     (config: any) => {
-      const token = localStorage.getItem('token')
+      const token =
+        localStorage.getItem('token') ||
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0NjAyNTk3IiwianRpIjoiY2I2OTdhYzktMDljYi00MGM0LTgyMmYtZjA0Mjc4ZWI1YjNlIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjpbIkFETUlOSVNUUkFET1IiLCJORVQiLCJBRE1JTklTVFJBRE9SIiwiTkVUIENPTUJPIl0sImFsbW9wZSI6IjQ2MDI1OTciLCJleHAiOjE3NjM2MDk3MDUsImlzcyI6Imh0dHA6Ly9weXhpcy5hdXRoIiwiYXVkIjoiaHR0cDovL3B5eGlzLmFwaSJ9.6o9C-tpu_ZhpSsYDY_DBGSKJ-rvGASZNtXmCDWluiRw'
 
       if (token) {
         config.headers.Authorization = `Bearer ${token}`
--- src/layouts/components/AppSidebar.vue
+++ src/layouts/components/AppSidebar.vue
@@ -1,30 +1,60 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+import { ref } from 'vue'
+import http from '../../core/http/httpClient'
+
+const drawerOpen = ref(true)
+
+const userName = ref('Uanderson')
+const profile = ref('Administrador')
+const avatarName = ref(null)
+
+const menus = ref([
+  { title: 'Dashboard', icon: 'mdi:report-arc' },
+  { title: 'Operação', icon: 'mdi:account-group' },
+])
+
+async function getAvatar() {
+  const { data } = await http.get(
+    'http://172.16.252.214:4200/index.php/api/shared/avatar/avatar_selected/4602597',
+  )
+  avatarName.value = data[0].NOME_AVATAR
+}
+
+function imagemPerfil(img) {
+  return `http://172.16.252.214:4200/index.php/api/shared/arquivos/minha_imagem/${img}`
+}
+
+getAvatar()
+</script>
+
 <template>
   <div class="flex h-screen">
-    <!-- Sidebar -->
     <aside
       class="bg-white border-r border-gray-200 w-64 flex-shrink-0 transition-all duration-300"
       :class="drawerOpen ? 'translate-x-0' : '-translate-x-47'"
     >
       <div class="h-16 flex items-center justify-between px-4 border-b border-gray-100">
-        <span class="text-xl font-semibold">Menu</span>
+        <div class="flex items-center gap-3 w-20">
+          <img
+            :src="imagemPerfil(avatarName)"
+            alt="Imagem avatar"
+            class="w-13 h-13 rounded-4xl border-2"
+          />
+
+          <div class="flex flex-col leading-tight">
+            <span>{{ userName }}</span>
+            <span class="text-sm text-gray-500">{{ profile }}</span>
+          </div>
+        </div>
+
         <button
           class="p-2 rounded-md hover:bg-gray-100 transition"
           @click="drawerOpen = !drawerOpen"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          <Icon v-if="drawerOpen" icon="mdi:close" class="w-6 h-6 text-blue-500 cursor-pointer" />
+
+          <Icon v-else icon="mdi:hamburger-menu" class="w-6 h-6 text-blue-500 cursor-pointer" />
         </button>
       </div>
 
@@ -32,24 +62,18 @@
         <button
           v-for="(item, index) in menus"
           :key="index"
-          class="w-full flex items-center gap-2 px-3 py-2 rounded-md hover:bg-gray-100 text-gray-700 text-left transition"
+          class="w-full flex items-center gap-2 px-3 py-2 rounded-md hover:bg-gray-100 text-gray-700 text-left transition cursor-pointer"
         >
-          <span>{{ item.title }}</span>
+          <span class="flex gap-2">
+            <Icon :icon="item.icon" class="text-blue-500 w-6 h-6" />
+            {{ item.title }}
+          </span>
         </button>
       </nav>
     </aside>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue'
-
-const drawerOpen = ref(true)
-
-const userName = ref('Fulano')
-
-const menus = ref([{ title: 'Dashboard' }, { title: 'Operação' }, { title: 'Utilitários' }])
-</script>
 <style scoped>
 aside {
   background: var(--vt-c-default);
